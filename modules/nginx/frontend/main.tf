d89f7c1444589620aeb72cf0e4db15b6d550464b
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-06-04 14:54:24 +0100

    Add nginx module for experience site

---
+++ modules/nginx/frontend/main.tf
@@ -0,0 +1,21 @@
+module "nginx_container" {
+  source = "../../../modules/container_definition"
+
+  image = local.image
+  name  = local.container_name
+
+  memory_reservation = var.memory_reservation
+
+  environment = {
+    APP_HOST = "localhost"
+    APP_PORT = var.forward_port
+  }
+
+  port_mappings = [{
+    containerPort = 9000,
+    hostPort      = 9000,
+    protocol      = "tcp"
+  }]
+
+  log_configuration = var.log_configuration
+}
---
+++ modules/nginx/frontend/variables.tf
@@ -0,0 +1,26 @@
+variable "forward_port" {
+  type = number
+}
+
+variable "memory_reservation" {
+  type    = number
+  default = 50
+}
+
+variable "log_configuration" {
+  type = object({
+    logDriver = string
+    options   = map(string)
+    secretOptions = list(object({
+      name      = string
+      valueFrom = string
+    }))
+  })
+
+  default = null
+}
+
+variable "container_tag" {
+  type    = string
+  default = "28e3e1b1cd4e8ad69ff44f28757eedff9c99a661"
+}
