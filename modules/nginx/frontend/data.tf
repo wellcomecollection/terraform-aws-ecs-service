55ea3d45c23c99535f711fee3f0f1cf934bdfc5e
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-06-29 14:36:49 +0100

    Remove dependency on remote state

--- modules/nginx/frontend/data.tf
+++ modules/nginx/frontend/data.tf
@@ -2,7 +2,6 @@ locals {
   container_name = "nginx"
   container_port = 80
 
-
   // See https://github.com/wellcomecollection/platform-infrastructure/tree/master/containers
   ecr_repo = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome/nginx_experience"
   image    = "${local.ecr_repo}:${var.container_tag}"
