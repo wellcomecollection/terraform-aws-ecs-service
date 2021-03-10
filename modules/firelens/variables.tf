425be018ddc150166d669d28d866c7a8ed0bb5ca
Alex Chan <alex@alexwlchan.net>
2021-03-10 19:04:43 +0000

    Allow using the PrivateLink endpoint in logging

--- modules/firelens/variables.tf
+++ modules/firelens/variables.tf
@@ -6,3 +6,9 @@ variable "container_tag" {
   type    = string
   default = "2ccd2c68f38aa77a8ac1a32fe3ea54bbbd397a38"
 }
+
+variable "use_privatelink_endpoint" {
+  description = "Should we send logs to Elasticsearch using the PrivateLink endpoint?  This must be accompanied by the appropriate security group."
+  type        = bool
+  default     = false
+}
