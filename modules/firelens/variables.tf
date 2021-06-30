3681e8398d2b01060aef46c6b745f043406fb6c0
Alex Chan <a.chan@wellcome.org>
2021-06-30 14:24:25 +0100

    Make the container registry configurable, allow pulling from ECR Public

--- modules/firelens/variables.tf
+++ modules/firelens/variables.tf
@@ -2,6 +2,19 @@ variable "namespace" {
   type = string
 }
 
+variable "container_registry" {
+  type        = string
+  description = "URL to the container registry for the logging image"
+
+  # See https://github.com/wellcomecollection/platform-infrastructure/tree/master/containers
+  default = "760097843905.dkr.ecr.eu-west-1.amazonaws.com/uk.ac.wellcome"
+}
+
+variable "container_name" {
+  type    = string
+  default = "fluentbit"
+}
+
 variable "container_tag" {
   type    = string
   default = "2ccd2c68f38aa77a8ac1a32fe3ea54bbbd397a38"
