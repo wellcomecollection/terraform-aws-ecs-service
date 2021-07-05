3ad5033d53cb18e7bdacaf6b6ceb54d8f7fbce9d
Alex Chan <a.chan@wellcome.org>
2021-07-05 08:08:43 +0100

    Don't look up secrets from Secrets Manager, just construct the ARN ourselves

--- modules/secret_arns/main.tf
+++ modules/secret_arns/main.tf
@@ -1,11 +1,27 @@
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
+
 locals {
-  // If a secret name has a colon in it, then it is a reference to a specific key within a JSON
-  // object secret: before the colon is the secret name, after is the key. This module provides
-  // a means to construct the correct ARNs for secret names both with and without specific key
-  // references.
-  //
-  // See "Example referencing a specific key within a secret" here:
-  // https://docs.aws.amazon.com/AmazonECS/latest/userguide/specifying-sensitive-data-secrets.html#secrets-examples
+  # If a secret name has a colon in it, then it is a reference to
+  # a specific key within a JSON object secret.
+  #
+  # e.g. if you have the secret
+  #
+  #     SecretId = credentials
+  #     SecretValue = {"username": "wellcome", "password": "s3kr1t!"}
+  #
+  # then the secret "credentials:username" would refer to "wellcome"
+  # and "credentials:password" would refer to "s3kr1t!".
+  #
+  # This module provides a way to construct the correct ARNs for secrets
+  # both with and without references.
+  #
+  # Note: we construct the ARNs by hand rather than using a "data" block
+  # so the secrets don't need to exist when we're planning, e.g. if we're
+  # creating secrets alongside the ECS service.
+  #
+  # See "Example referencing a specific key within a secret" here:
+  # https://docs.aws.amazon.com/AmazonECS/latest/userguide/specifying-sensitive-data-secrets.html
   secret_reference_parts = {
     for label, secret in var.secrets : label => split(":", secret)
   }
@@ -16,17 +32,16 @@ locals {
     }
   }
 
+  aws_region = data.aws_region.current.name
+  account_id = data.aws_caller_identity.current.account_id
+
   arns_without_keys = {
-    for label, secret in local.secret_references : label => data.aws_secretsmanager_secret.for_service[label].arn
+    for label, secret in local.secret_references : label =>
+      "arn:aws:secretsmanager:${local.aws_region}:${local.account_id}:secret:${secret["name"]}"
   }
 
   arns_with_keys = {
     for label, secret in local.secret_references : label =>
     secret["key"] == "" ? local.arns_without_keys[label] : "${local.arns_without_keys[label]}:${secret["key"]}::"
   }
-}
-
-data "aws_secretsmanager_secret" "for_service" {
-  for_each = local.secret_references
-  name     = each.value["name"]
 }
