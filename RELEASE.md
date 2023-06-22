861d3e79bef72635283f39df2cc6b03310e132c2
Alex Chan <alex@alexwlchan.net>
2023-06-22 15:20:07 +0100

    also export the arn from the service module

---
+++ RELEASE.md
@@ -0,0 +1,3 @@
+RELEASE_TYPE: minor
+
+Also export the ARN from the service module.
--- modules/service/outputs.tf
+++ modules/service/outputs.tf
@@ -1,3 +1,7 @@
+output "arn" {
+  value = aws_ecs_service.service.arn
+}
+
 output "name" {
   value = aws_ecs_service.service.name
 }
